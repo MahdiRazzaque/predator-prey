21082b1037f33e9e0a30e5701e5de40044099283
MahdiRazzaque <mahdi.razzaque+github@gmail.com>
2025-02-04 18:03:33 +0000

    Fix

--- Rabbit.java
+++ Rabbit.java
@@ -1,4 +1,3 @@
-import java.util.List;
 import java.util.HashMap;
 
 /**
@@ -8,19 +7,12 @@ import java.util.HashMap;
  * @author David J. Barnes and Michael Kölling
  * @version 7.1
  */
-public class Rabbit extends Animal
-{
+public class Rabbit extends Animal {
     // Characteristics shared by all rabbits (class variables).
-    // The age at which a rabbit can start to breed.
     private static final int BREEDING_AGE = 5;
-    // The age to which a rabbit can live.
     private static final int MAX_AGE = 40;
-    // The likelihood of a rabbit breeding.
     private static final double BREEDING_PROBABILITY = 0.12;
-    // The maximum number of births.
     private static final int MAX_LITTER_SIZE = 4;
-    // Food value when eaten by a fox
-    private static final int FOOD_VALUE = 5;
 
     /**
      * Create a new rabbit. A rabbit may be created with age
@@ -29,101 +21,8 @@ public class Rabbit extends Animal
      * @param randomAge If true, the rabbit will have a random age.
      * @param location The location within the field.
      */
-    public Rabbit(boolean randomAge, Location location)
-    {
+    public Rabbit(boolean randomAge, Location location) {
         super(randomAge, location, BREEDING_AGE, MAX_AGE, BREEDING_PROBABILITY, MAX_LITTER_SIZE, 
               new HashMap<>(), Rabbit.class);
-    }
-    
-    /**
-     * This is what the rabbit does most of the time - it runs 
-     * around. Sometimes it will breed or die of old age.
-     * @param currentField The field currently occupied.
-     * @param nextFieldState The field to move to.
-     */
-    @Override
-    public void act(Field currentField, Field nextFieldState)
-    {
-        super.act(currentField, nextFieldState);
-    }
-
-    @Override
-    public String toString() {
-        return "Rabbit{" +
-                "age=" + getAge() +
-                ", alive=" + isAlive() +
-                ", location=" + getLocation() +
-                '}';
-    }
-
-    /**
-     * Check whether this rabbit is to give birth at this step.
-     * New births will be made into free adjacent locations.
-     * @param nextFieldState The field to place the young rabbits in.
-     * @param freeLocations The list of free adjacent locations.
-     */
-    private void giveBirth(Field nextFieldState, List<Location> freeLocations)
-    {
-        // New rabbits are born into adjacent locations.
-        // Get a list of adjacent free locations.
-        int births = breed();
-        if(births > 0) {
-            for(int b = 0; b < births && !freeLocations.isEmpty(); b++) {
-                Location loc = freeLocations.remove(0);
-                Rabbit young = new Rabbit(false, loc);
-                nextFieldState.placeEntity(young, loc);
-            }
-        }
-    }
-        
-    /**
-     * Generate a number representing the number of births,
-     * if it can breed.
-     * @return The number of births (may be zero).
-     */
-    private int breed()
-    {
-        int births;
-        if(canBreed()) {
-            births = rand.nextInt(MAX_LITTER_SIZE) + 1;
-        }
-        else {
-            births = 0;
-        }
-        return births;
-    }
-
-    /**
-     * A rabbit can breed if it has reached the breeding age.
-     * @return true if the rabbit can breed, false otherwise.
-     */
-    private boolean canBreed()
-    {
-        return getAge() >= BREEDING_AGE;
-    }
-
-    @Override
-    protected int getBreedingAge() {
-        return BREEDING_AGE;
-    }
-
-    @Override
-    protected int getMaxAge() {
-        return MAX_AGE;
-    }
-
-    @Override
-    protected double getBreedingProbability() {
-        return BREEDING_PROBABILITY;
-    }
-
-    @Override
-    protected int getMaxLitterSize() {
-        return MAX_LITTER_SIZE;
-    }
-
-    @Override
-    protected int getFoodValue() {
-        return FOOD_VALUE;
     }
 }
