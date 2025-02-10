89a09250ce510decef2b83e6769729c941087037
MahdiRazzaque <mahdi.razzaque+github@gmail.com>
2025-02-10 11:19:25 +0000

    Use final variables

--- Berries.java
+++ Berries.java
@@ -1,5 +1,14 @@
 public class Berries extends Plant {
+    private static final String NAME = "Berries";
+    private static final int GROWTH_RATE = 15;
+    private static final int REPRODUCTION_RATE = 50;
+    private static final int LIFE_SPAN = 250;
+    private static final int SPREAD_RATE = 1;
+    private static final int GROWTH_START_HOUR = 18;
+    private static final int GROWTH_END_HOUR = 6;
+    private static final int GROWTH_STAGE = 0;
+
     public Berries(Location location, Simulator simulator, Time time) {
-        super("Berries", 15, 50, 250, 1, 18, 6, 0, location, simulator, time, Berries.class);
+        super(NAME, GROWTH_RATE, REPRODUCTION_RATE, LIFE_SPAN, SPREAD_RATE, GROWTH_START_HOUR, GROWTH_END_HOUR, GROWTH_STAGE, location, simulator, time, Berries.class);
     }
 }
--- Seeds.java
+++ Seeds.java
@@ -1,5 +1,13 @@
 public class Seeds extends Plant {
+    private static final String NAME = "Seeds";
+    private static final int GROWTH_RATE = 5;
+    private static final int REPRODUCTION_RATE = 15;
+    private static final int LIFE_SPAN = -1;
+    private static final int SPREAD_RATE = 2;
+    private static final int GROWTH_START_HOUR = 6;
+    private static final int GROWTH_END_HOUR = 18;
+    private static final int GROWTH_STAGE = 0;
     public Seeds(Location location, Simulator simulator, Time time) {
-        super("Seeds", 5, 15, -1, 2, 6, 18, 0, location, simulator, time, Seeds.class);
-    }
+        super(NAME, GROWTH_RATE, REPRODUCTION_RATE, LIFE_SPAN, SPREAD_RATE, GROWTH_START_HOUR, GROWTH_END_HOUR, GROWTH_STAGE, location, simulator, time, Berries.class);
+        }
 }
